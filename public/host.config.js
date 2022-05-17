846f354d8d313c478d5ee2b7f289d2621065275f
jswinter <winterleave01@gmail.com>
2022-05-17 17:02:54 +0800

    添加public config

---
+++ public/host.config.js
@@ -0,0 +1,11 @@
+/*
+ * @Description: 服务器域名配置
+ * @Date: 2021-09-11 08:14:17
+ * @LastEditors: Alan
+ * @LastEditTime: 2021-10-21 22:22:08
+ * @FilePath: /vue-casino-admin/public/host.config.js
+ */
+const VUE_APP_BASE_API = {
+    baseUrl: "http://searchaccount.kk88815.net",
+};
+
